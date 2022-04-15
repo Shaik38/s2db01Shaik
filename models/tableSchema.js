a414c0584fd88e8305f68771e5f2e8ce4298cadd
Abdul K S Shaik <s546901@nwmissouri.edu>
2022-04-15 00:35:45 -0500

    app.js

---
+++ models/tableSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const tableSchema = mongoose.Schema({
+table_color: String,
+table_size: String,
+table_num: Number
+})
+module.exports = mongoose.model("tables",
+tableSchema)
